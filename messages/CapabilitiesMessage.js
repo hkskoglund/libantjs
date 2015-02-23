54dc22b4ee54e8fbe7c6dfa174e57728f0be98b5
Henning Knut Skoglund <hkskoglund@gmail.com>
2015-02-23 10:50:55 +0100

    Using +=enabled -=disabled for capabilities

--- messages/CapabilitiesMessage.js
+++ messages/CapabilitiesMessage.js
@@ -51,10 +51,8 @@ define(function (require, exports, module){
 
         this.maxSensRcoreChannels =  this.content[5];
 
-
-
         this.standardOptions = {
-            value: "MSB " + standardOptions.toString(2) + " " + standardOptions,
+            value: standardOptions.toString(2) + "b = " + standardOptions,
             CAPABILITIES_NO_RECEIVE_CHANNELS : standardOptions & 0x01,
             CAPABILITIES_NO_TRANSMIT_CHANNELS : standardOptions & 0x02,
             CAPABILITIES_NO_RECEIVE_MESSAGES : standardOptions & (1 << 3),
@@ -64,7 +62,7 @@ define(function (require, exports, module){
         };
 
         this.advancedOptions = {
-            value: "MSB " + advancedOptions.toString(2) + " " + advancedOptions,
+            value: advancedOptions.toString(2) + " = " + advancedOptions,
             CAPABILITIES_NETWORK_ENABLED : advancedOptions & 0x02,
             CAPABILITIES_SERIAL_NUMBER_ENABLED : advancedOptions & (1 << 3),
             CAPABILITIES_PER_CHANNEL_TX_POWER_ENABLED : advancedOptions & (1 << 4),
@@ -75,7 +73,7 @@ define(function (require, exports, module){
 
         if (advancedOptions2 !== undefined)
         this.advancedOptions2 = {
-            value: "MSB " + advancedOptions2.toString(2) + " " + advancedOptions2,
+            value: advancedOptions2.toString(2) + "b = " + advancedOptions2,
             CAPABILITIES_LED_ENABLED : advancedOptions2 & 0x01,
             CAPABILITIES_EXT_MESSAGE_ENABLED : advancedOptions2 & 0x02,
             CAPABILITIES_SCAN_MODE_ENABLED : advancedOptions2 & (1 << 2),
@@ -85,10 +83,10 @@ define(function (require, exports, module){
         };
 
         // ANT USB 2 does not have advanced options 3, so it will be undefined
-        
+
         if (advancedOptions3 !== undefined)
             this.advancedOptions3 = {
-                value : "MSB "+advancedOptions3.toString(2) + " " + advancedOptions3,
+                value : advancedOptions3.toString(2) + "b = " + advancedOptions3,
                 CAPABILITIES_ADVANCED_BURST_ENABLED : advancedOptions3 & 0x01,
                 CAPABILITIES_EVENT_BUFFERING_ENABLED : advancedOptions3 & 0x02,
                 CAPABILITIES_EVENT_FILTERING_ENABLED : advancedOptions3 & (1 << 2),
@@ -101,79 +99,85 @@ define(function (require, exports, module){
 
         if (advancedOptions4 !== undefined)
             this.advancedOptions4 = {
-                value: "MSB " + advancedOptions4.toString(2) + " " + advancedOptions4,
+                value: "MSB " + advancedOptions4.toString(2) + "b " + advancedOptions4,
                 CAPABILITIES_RFACTIVE_NOTIFICATION_ENABLED: advancedOptions4 & 0x01 // Bit 0
                 // Bit 1-7 reserved
             };
 
     };
 
-    CapabilitiesMessage.prototype.toString = function (){
+    CapabilitiesMessage.prototype.toString = function ()    {
 
         var msg = Message.prototype.toString.call(this) + " Channels " + this.getNumberOfChannels() + " | Networks " + this.getNumberOfNetworks()+ ' | sensRcore channels ' + this.maxSensRcoreChannels+' | ';
 
-        if (this.standardOptions.CAPABILITIES_NO_RECEIVE_CHANNELS)
-            msg += "No receive channels | ";
-        if (this.standardOptions.CAPABILITIES_NO_TRANSMIT_CHANNELS)
-            msg += "No transmit channels | ";
-        if (this.standardOptions.CAPABILITIES_NO_RECEIVE_MESSAGES)
-            msg += "No receive messages | ";
-        if (this.standardOptions.CAPABILITIES_NO_TRANSMIT_MESSAGES)
-            msg += "No transmit messages | " ;
-        if (this.standardOptions.CAPABILITIES_NO_ACKD_MESSAGES)
-            msg += "No ackd. messages | ";
-        if (this.standardOptions.CAPABILITIES_NO_BURST_MESSAGES)
-            msg += "No burst messages | ";
+        msg += (this.standardOptions.CAPABILITIES_NO_RECEIVE_CHANNELS ? '+' : '-')+ "No receive channels | ";
 
-        if (this.advancedOptions.CAPABILITIES_NETWORK_ENABLED)
-            msg += "Network | ";
-        if (this.advancedOptions.CAPABILITIES_SERIAL_NUMBER_ENABLED)
-            msg += "Serial number | ";
-        if (this.advancedOptions.CAPABILITIES_PER_CHANNEL_TX_POWER_ENABLED)
-            msg += "Per channel Tx Power | ";
-        if (this.advancedOptions.CAPABILITIES_LOW_PRIORITY_SEARCH_ENABLED)
-            msg += "Low priority search | " ;
-        if (this.advancedOptions.CAPABILITIES_SCRIPT_ENABLED)
-            msg += "Script | ";
-        if (this.advancedOptions.CAPABILITIES_SEARCH_LIST_ENABLED)
-            msg += "Search list | ";
+        msg += (this.standardOptions.CAPABILITIES_NO_TRANSMIT_CHANNELS ? '+' : '-')+  "No transmit channels | ";
 
-        if (this.advancedOptions2){
-            if (this.advancedOptions2.CAPABILITIES_LED_ENABLED)
-                msg += "Led | ";
-            if (this.advancedOptions2.CAPABILITIES_EXT_MESSAGE_ENABLED)
-                msg += "Extended messages | ";
-            if (this.advancedOptions2.CAPABILITIES_SCAN_MODE_ENABLED)
-                msg += "Scan mode | ";
-            if (this.advancedOptions2.CAPABILITIES_PROXY_SEARCH_ENABLED)
-                msg += "Proximity search | ";
-            if (this.advancedOptions2.CAPABILITIES_EXT_ASSIGN_ENABLED)
-                msg += "Extended assign | ";
-            if (this.advancedOptions2.CAPABILITIES__FS_ANTFS_ENABLED)
-                msg += "ANT-FS | ";
+        msg += (this.standardOptions.CAPABILITIES_NO_RECEIVE_MESSAGES ? '+' : '-')+ "No receive messages | ";
+
+        msg +=(this.standardOptions.CAPABILITIES_NO_TRANSMIT_MESSAGES ? '+' : '-')+ "No transmit messages | " ;
+
+        msg += (this.standardOptions.CAPABILITIES_NO_ACKD_MESSAGES ? '+' : '-')+ "No ackd. messages | ";
+
+        msg += (this.standardOptions.CAPABILITIES_NO_BURST_MESSAGES ? '+' : '-')+ "No burst messages | ";
+
+
+        msg += (this.advancedOptions.CAPABILITIES_NETWORK_ENABLED ? '+' : '-')+ "Network | ";
+
+        msg += (this.advancedOptions.CAPABILITIES_SERIAL_NUMBER_ENABLED ? '+' : '-')+ "Serial number | ";
+
+        msg += (this.advancedOptions.CAPABILITIES_PER_CHANNEL_TX_POWER_ENABLED ? '+' : '-')+ "Per channel Tx Power | ";
+
+        msg += (this.advancedOptions.CAPABILITIES_LOW_PRIORITY_SEARCH_ENABLED ? '+' : '-')+ "Low priority search | " ;
+
+        msg += (this.advancedOptions.CAPABILITIES_SCRIPT_ENABLED ? '+' : '-')+ "Script | ";
+
+        msg += (this.advancedOptions.CAPABILITIES_SEARCH_LIST_ENABLED ? '+' : '-')+ "Search list | ";
+
+
+        if (this.advancedOptions2)        {
+          msg +=(this.advancedOptions2.CAPABILITIES_LED_ENABLED ? '+' : '-')+  "Led | ";
+
+          msg += (this.advancedOptions2.CAPABILITIES_EXT_MESSAGE_ENABLED ? '+' : '-')+ "Extended messages | ";
+
+          msg += (this.advancedOptions2.CAPABILITIES_SCAN_MODE_ENABLED ? '+' : '-')+ "Scan mode | ";
+
+          msg += (this.advancedOptions2.CAPABILITIES_PROXY_SEARCH_ENABLED ? '+' : '-')+ "Proximity search | ";
+
+          msg += (this.advancedOptions2.CAPABILITIES_EXT_ASSIGN_ENABLED ? '+' : '-')+"Extended assign | ";
+
+          msg += (this.advancedOptions2.CAPABILITIES__FS_ANTFS_ENABLED ? '+' : '-')+ "ANT-FS | ";
+
         }
 
-        if (this.advancedOptions3){
-            if (this.advancedOptions3.CAPABILITIES_ADVANCED_BURST_ENABLED)
-                msg += "Advanced burst | ";
-            if (this.advancedOptions3.CAPABILITIES_EVENT_BUFFERING_ENABLED)
-                msg += "Event buffering | ";
-            if (this.advancedOptions3.CAPABILITIES_EVENT_FILTERING_ENABLED)
-                msg += "Event filtering | ";
-            if (this.advancedOptions3.CAPABILITIES_HIGH_DUTY_SEARCH_ENABLED)
-                msg += "High duty search | ";
-            if (this.advancedOptions3.CAPABILITIES_SEARCH_SHARING_ENABLED)
-                    msg += "Search sharing | ";
-            if (this.advancedOptions3.CAPABILITIES_ENCRYPTED_CHANNEL_ENABLED)
-                    msg += "Encrypted channel | ";
-            if (this.advancedOptions3.CAPABILITIES_SELECTIVE_DATA_ENABLED)
-                msg += "Selective data | ";
+        if (this.advancedOptions3)        {
+            msg +=  (this.advancedOptions3.CAPABILITIES_ADVANCED_BURST_ENABLED ? '+' : '-')+ 'Advanced burst |';
+
+            msg += (this.advancedOptions3.CAPABILITIES_EVENT_BUFFERING_ENABLED ? '+' : '-')+"Event buffering | ";
+
+            msg += (this.advancedOptions3.CAPABILITIES_EVENT_FILTERING_ENABLED ? '+' : '-')+"Event filtering | ";
+
+            msg += (this.advancedOptions3.CAPABILITIES_HIGH_DUTY_SEARCH_ENABLED ? '+' : '-')+"High duty search | ";
+
+            msg +=  (this.advancedOptions3.CAPABILITIES_SEARCH_SHARING_ENABLED ? '+' : '-')+"Search sharing | ";
+
+            msg += (this.advancedOptions3.CAPABILITIES_ENCRYPTED_CHANNEL_ENABLED ? '+' : '-')+"Encrypted channel | ";
+
+            msg += (this.advancedOptions3.CAPABILITIES_SELECTIVE_DATA_ENABLED  ? '+' : '-')+"Selective data update | ";
+
         }
 
-        if (this.advancedOptions4){
-            if (this.advancedOptions4.CAPABILITIES_RFACTIVE_NOTIFICATION_ENABLED)
-                msg += " RF Active notification | ";
+        if (this.advancedOptions4)        {
+          msg += (this.advancedOptions4.CAPABILITIES_RFACTIVE_NOTIFICATION_ENABLED ? '+' : '-')+"RF Active notification | ";
+
         }
+
+        if (this.standardOptions !== undefined) msg += 'S.O '+this.standardOptions.value+ ' |';
+        if (this.advancedOptions !== undefined) msg += ' A.O '+this.advancedOptions.value+'|';
+        if (this.advancedOptions2 !== undefined) msg += ' A.O2 '+this.advancedOptions2.value+'|';
+        if (this.advancedOptions3 !== undefined) msg += ' A.O3 '+this.advancedOptions3.value+'|';
+        if (this.advancedOptions4 !== undefined) msg += ' A.O4 '+this.advancedOptions4.value;
 
         return msg;
     };
