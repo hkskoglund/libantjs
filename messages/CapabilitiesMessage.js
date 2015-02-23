5effb842e73a8582bac363a63fa3cc1a57218dfe
Henning Knut Skoglund <hkskoglund@gmail.com>
2015-02-23 09:08:52 +0100

    Fixed capabilities advanced options 3

--- messages/CapabilitiesMessage.js
+++ messages/CapabilitiesMessage.js
@@ -8,12 +8,9 @@ define(function (require, exports, module){
 
     var Message = require('./Message');
 
-    function CapabilitiesMessage(data){
+    function CapabilitiesMessage(data)    {
 
       Message.call(this,data);
-
-        if (data)
-            this.decode();
 
     }
 
@@ -54,8 +51,7 @@ define(function (require, exports, module){
 
         this.maxSensRcoreChannels =  this.content[5];
 
-       // ANT USB 2 does not have advanced options 3, so it will be undefined
-        advancedOptions3 = this.content[6];
+
 
         this.standardOptions = {
             value: "MSB " + standardOptions.toString(2) + " " + standardOptions,
@@ -88,6 +84,8 @@ define(function (require, exports, module){
             CAPABILITIES_FS_ANTFS_ENABLED : advancedOptions2 & (1 << 6), // (1 << n) = set bit n high (bit numbered from 0 - n)
         };
 
+        // ANT USB 2 does not have advanced options 3, so it will be undefined
+        
         if (advancedOptions3 !== undefined)
             this.advancedOptions3 = {
                 value : "MSB "+advancedOptions3.toString(2) + " " + advancedOptions3,
@@ -95,7 +93,10 @@ define(function (require, exports, module){
                 CAPABILITIES_EVENT_BUFFERING_ENABLED : advancedOptions3 & 0x02,
                 CAPABILITIES_EVENT_FILTERING_ENABLED : advancedOptions3 & (1 << 2),
                 CAPABILITIES_HIGH_DUTY_SEARCH_ENABLED : advancedOptions3 & (1 << 3),
-                CAPABILITIES_SELECTIVE_DATA_ENABLED : advancedOptions3 & (1 << 6)
+                CAPABILITIES_SEARCH_SHARING_ENABLED : advancedOptions3 & (1 << 4),
+
+                CAPABILITIES_SELECTIVE_DATA_ENABLED : advancedOptions3 & (1 << 6),
+                CAPABILITIES_ENCRYPTED_CHANNEL_ENABLED : advancedOptions3 & (1 << 7)
             };
 
         if (advancedOptions4 !== undefined)
@@ -106,63 +107,6 @@ define(function (require, exports, module){
             };
 
     };
-
-    //CapabilitiesMessage.prototype.showCapabilities = function ()
-    //{
-    //    console.log("Capabilities\n------------");
-    //    console.log("Max channels " + this.getNumberOfChannels());
-    //    console.log("Max network " + this.getNumberOfNetworks());
-
-    //    if (this.standardOptions.CAPABILITIES_NO_RECEIVE_CHANNELS)
-    //        console.log("No receive channels");
-    //    if (this.standardOptions.CAPABILITIES_NO_TRANSMIT_CHANNELS)
-    //        console.log("No transmit channels");
-    //    if (this.standardOptions.CAPABILITIES_NO_RECEIVE_MESSAGES)
-    //        console.log("No receive messages");
-    //    if (this.standardOptions.CAPABILITIES_NO_TRANSMIT_MESSAGES)
-    //        console.log("No transmit messages");
-    //    if (this.standardOptions.CAPABILITIES_NO_ACKD_MESSAGES)
-    //        console.log("No ackd. messages");
-    //    if (this.standardOptions.CAPABILITIES_NO_BURST_MESSAGES)
-    //        console.log("No burst messages");
-
-    //    if (this.advancedOptions.CAPABILITIES_NETWORK_ENABLED)
-    //        console.log("Network");
-    //    if (this.advancedOptions.CAPABILITIES_SERIAL_NUMBER_ENABLED)
-    //        console.log("Serial number");
-    //    if (this.advancedOptions.CAPABILITIES_PER_CHANNEL_TX_POWER_ENABLED)
-    //        console.log("Per channel Tx Power");
-    //    if (this.advancedOptions.CAPABILITIES_LOW_PRIORITY_SEARCH_ENABLED)
-    //        console.log("Low priority search");
-    //    if (this.advancedOptions.CAPABILITIES_SCRIPT_ENABLED)
-    //        console.log("Script");
-    //    if (this.advancedOptions.CAPABILITIES_SEARCH_LIST_ENABLED)
-    //        console.log("Search list");
-
-    //    if (this.advancedOptions2.CAPABILITIES_LED_ENABLED)
-    //        console.log("Led");
-    //    if (this.advancedOptions2.CAPABILITIES_EXT_MESSAGE_ENABLED)
-    //        console.log("Extended messages");
-    //    if (this.advancedOptions2.CAPABILITIES_SCAN_MODE_ENABLED)
-    //        console.log("Scan mode");
-    //    if (this.advancedOptions2.CAPABILITIES_PROXY_SEARCH_ENABLED)
-    //        console.log("Proximity search");
-    //    if (this.advancedOptions2.CAPABILITIES_EXT_ASSIGN_ENABLED)
-    //        console.log("Extended assign");
-    //    if (this.advancedOptions2.CAPABILITIES__FS_ANTFS_ENABLED)
-    //        console.log("ANT-FS");
-
-    //    if (this.advancedOptions3.CAPABILITIES_ADVANCED_BURST_ENABLED)
-    //        console.log("Advanced burst");
-    //    if (this.advancedOptions3.CAPABILITIES_EVENT_BUFFERING_ENABLED)
-    //        console.log("Event buffering");
-    //    if (this.advancedOptions3.CAPABILITIES_EVENT_FILTERING_ENABLED)
-    //        console.log("Event filtering");
-    //    if (this.advancedOptions3.CAPABILITIES_HIGH_DUTY_SEARCH_ENABLED)
-    //        console.log("High duty search");
-    //    if (this.advancedOptions3.CAPABILITIES_SELECTIVE_DATA_ENABLED)
-    //        console.log("Selective data");
-    //}
 
     CapabilitiesMessage.prototype.toString = function (){
 
@@ -218,6 +162,10 @@ define(function (require, exports, module){
                 msg += "Event filtering | ";
             if (this.advancedOptions3.CAPABILITIES_HIGH_DUTY_SEARCH_ENABLED)
                 msg += "High duty search | ";
+            if (this.advancedOptions3.CAPABILITIES_SEARCH_SHARING_ENABLED)
+                    msg += "Search sharing | ";
+            if (this.advancedOptions3.CAPABILITIES_ENCRYPTED_CHANNEL_ENABLED)
+                    msg += "Encrypted channel | ";
             if (this.advancedOptions3.CAPABILITIES_SELECTIVE_DATA_ENABLED)
                 msg += "Selective data | ";
         }
