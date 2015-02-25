6b7f0629b800983063e7c7515eff9eb4e785a354
Henning Knut Skoglund <hkskoglund@gmail.com>
2015-02-25 16:47:51 +0100

    Simplified capabilities

--- messages/requested/CapabilitiesMessage.js
+++ messages/requested/CapabilitiesMessage.js
@@ -18,7 +18,6 @@ define(function (require, exports, module){
 
     CapabilitiesMessage.prototype.constructor = CapabilitiesMessage;
 
-
     // Inspired by Dynastream Android SDK 4.0.0
     CapabilitiesMessage.prototype.getNumberOfChannels = function ()
     {
@@ -37,72 +36,62 @@ define(function (require, exports, module){
         this.MAX_CHAN =  this.content[0];
         this.MAX_NET = this.content[1];
 
-        var standardOptions = this.content[2],
+        this.standardOptions = this.content[2];
 
-            advancedOptions = this.content[3],
+        this.advancedOptions = this.content[3];
 
         // Documentation update http://www.thisisant.com/forum/viewthread/4250/
 
-            advancedOptions2 = this.content[4],
+        this.advancedOptions2 = this.content[4];
 
-            advancedOptions3 = this.content[6],
+        this.advancedOptions3 = this.content[6];
 
-            advancedOptions4 = this.content[7];
+        this.advancedOptions4 = this.content[7];
 
         this.maxSensRcoreChannels =  this.content[5];
 
-        this.standardOptions = {
-            value: standardOptions.toString(2) + "b = " + standardOptions,
-            CAPABILITIES_NO_RECEIVE_CHANNELS : standardOptions & 0x01,
-            CAPABILITIES_NO_TRANSMIT_CHANNELS : standardOptions & 0x02,
-            CAPABILITIES_NO_RECEIVE_MESSAGES : standardOptions & (1 << 3),
-            CAPABILITIES_NO_TRANSMIT_MESSAGES : standardOptions & (1 << 4),
-            CAPABILITIES_NO_ACKD_MESSAGES : standardOptions & (1 << 5),
-            CAPABILITIES_NO_BURST_MESSAGES : standardOptions & (1 << 6),
-        };
+        this.NO_RECEIVE_CHANNELS = this.standardOptions & 0x01;
+        this.NO_TRANSMIT_CHANNELS = this.standardOptions & 0x02;
+        this.NO_RECEIVE_MESSAGES = this.standardOptions & (1 << 3);
+        this.NO_TRANSMIT_MESSAGES = this.standardOptions & (1 << 4);
+        this.NO_ACKD_MESSAGES = this.standardOptions & (1 << 5);
+        this.NO_BURST_MESSAGES = this.standardOptions & (1 << 6);
 
-        this.advancedOptions = {
-            value: advancedOptions.toString(2) + " = " + advancedOptions,
-            CAPABILITIES_NETWORK_ENABLED : advancedOptions & 0x02,
-            CAPABILITIES_SERIAL_NUMBER_ENABLED : advancedOptions & (1 << 3),
-            CAPABILITIES_PER_CHANNEL_TX_POWER_ENABLED : advancedOptions & (1 << 4),
-            CAPABILITIES_LOW_PRIORITY_SEARCH_ENABLED : advancedOptions & (1 << 5),
-            CAPABILITIES_SCRIPT_ENABLED : advancedOptions & (1 << 6),
-            CAPABILITIES_SEARCH_LIST_ENABLED : advancedOptions & (1 << 7),
-        };
+        this.NETWORK_ENABLED = this.advancedOptions & 0x02;
+        this.SERIAL_NUMBER_ENABLED = this.advancedOptions & (1 << 3);
+        this.PER_CHANNEL_TX_POWER_ENABLED = this.advancedOptions & (1 << 4);
+        this.LOW_PRIORITY_SEARCH_ENABLED = this.advancedOptions & (1 << 5);
+        this.SCRIPT_ENABLED = this.advancedOptions & (1 << 6);
+        this.SEARCH_LIST_ENABLED = this.advancedOptions & (1 << 7);
 
         if (advancedOptions2 !== undefined)
-        this.advancedOptions2 = {
-            value: advancedOptions2.toString(2) + "b = " + advancedOptions2,
-            CAPABILITIES_LED_ENABLED : advancedOptions2 & 0x01,
-            CAPABILITIES_EXT_MESSAGE_ENABLED : advancedOptions2 & 0x02,
-            CAPABILITIES_SCAN_MODE_ENABLED : advancedOptions2 & (1 << 2),
-            CAPABILITIES_PROXY_SEARCH_ENABLED : advancedOptions2 & (1 << 4),
-            CAPABILITIES_EXT_ASSIGN_ENABLED : advancedOptions2 & (1 << 5),
-            CAPABILITIES_FS_ANTFS_ENABLED : advancedOptions2 & (1 << 6), // (1 << n) = set bit n high (bit numbered from 0 - n)
-        };
+        {
+            this.LED_ENABLED = this.advancedOptions2 & 0x01;
+            this.EXT_MESSAGE_ENABLED = this.advancedOptions2 & 0x02;
+            this.SCAN_MODE_ENABLED = this.advancedOptions2 & (1 << 2);
+            this.PROXY_SEARCH_ENABLED = this.advancedOptions2 & (1 << 4);
+            this.EXT_ASSIGN_ENABLED = this.advancedOptions2 & (1 << 5);
+            this.FS_ANTFS_ENABLED = this.advancedOptions2 & (1 << 6); // (1 << n) = set bit n high (bit numbered from 0 - n)
+        }
 
         // ANT USB 2 does not have advanced options 3, so it will be undefined
 
         if (advancedOptions3 !== undefined)
-            this.advancedOptions3 = {
-                value : advancedOptions3.toString(2) + "b = " + advancedOptions3,
-                CAPABILITIES_ADVANCED_BURST_ENABLED : advancedOptions3 & 0x01,
-                CAPABILITIES_EVENT_BUFFERING_ENABLED : advancedOptions3 & 0x02,
-                CAPABILITIES_EVENT_FILTERING_ENABLED : advancedOptions3 & (1 << 2),
-                CAPABILITIES_HIGH_DUTY_SEARCH_ENABLED : advancedOptions3 & (1 << 3),
-                CAPABILITIES_SEARCH_SHARING_ENABLED : advancedOptions3 & (1 << 4),
-
-                CAPABILITIES_SELECTIVE_DATA_ENABLED : advancedOptions3 & (1 << 6),
-                CAPABILITIES_ENCRYPTED_CHANNEL_ENABLED : advancedOptions3 & (1 << 7)
-            };
+        {
+            this.ADVANCED_BURST_ENABLED = this.advancedOptions3 & 0x01;
+            this.EVENT_BUFFERING_ENABLED = this.advancedOptions3 & 0x02;
+            this.EVENT_FILTERING_ENABLED = this.advancedOptions3 & (1 << 2);
+            this.HIGH_DUTY_SEARCH_ENABLED = this.advancedOptions3 & (1 << 3);
+            this.SEARCH_SHARING_ENABLED = this.advancedOptions3 & (1 << 4);
+            this.SELECTIVE_DATA_ENABLED = this.advancedOptions3 & (1 << 6);
+            this.ENCRYPTED_CHANNEL_ENABLED = this.advancedOptions3 & (1 << 7);
+        }
 
         if (advancedOptions4 !== undefined)
-            this.advancedOptions4 = {
-                value: "MSB " + advancedOptions4.toString(2) + "b " + advancedOptions4,
-                CAPABILITIES_RFACTIVE_NOTIFICATION_ENABLED: advancedOptions4 & 0x01 // Bit 0
+        {
+                this.RFACTIVE_NOTIFICATION_ENABLED = this.advancedOptions4 & 0x01; // Bit 0
                 // Bit 1-7 reserved
-            };
+        }
 
     };
 
@@ -110,74 +99,74 @@ define(function (require, exports, module){
 
         var msg = Message.prototype.toString.call(this) + " Channels " + this.getNumberOfChannels() + " | Networks " + this.getNumberOfNetworks()+ ' | sensRcore channels ' + this.maxSensRcoreChannels+' | ';
 
-        msg += (this.standardOptions.CAPABILITIES_NO_RECEIVE_CHANNELS ? '+' : '-')+ "No receive channels | ";
+        msg += (this.standardOptions.NO_RECEIVE_CHANNELS ? '+' : '-')+ "No receive channels | ";
 
-        msg += (this.standardOptions.CAPABILITIES_NO_TRANSMIT_CHANNELS ? '+' : '-')+  "No transmit channels | ";
+        msg += (this.standardOptions.NO_TRANSMIT_CHANNELS ? '+' : '-')+  "No transmit channels | ";
 
-        msg += (this.standardOptions.CAPABILITIES_NO_RECEIVE_MESSAGES ? '+' : '-')+ "No receive messages | ";
+        msg += (this.standardOptions.NO_RECEIVE_MESSAGES ? '+' : '-')+ "No receive messages | ";
 
-        msg +=(this.standardOptions.CAPABILITIES_NO_TRANSMIT_MESSAGES ? '+' : '-')+ "No transmit messages | " ;
+        msg +=(this.standardOptions.NO_TRANSMIT_MESSAGES ? '+' : '-')+ "No transmit messages | " ;
 
-        msg += (this.standardOptions.CAPABILITIES_NO_ACKD_MESSAGES ? '+' : '-')+ "No ackd. messages | ";
+        msg += (this.standardOptions.NO_ACKD_MESSAGES ? '+' : '-')+ "No ackd. messages | ";
 
-        msg += (this.standardOptions.CAPABILITIES_NO_BURST_MESSAGES ? '+' : '-')+ "No burst messages | ";
+        msg += (this.standardOptions.NO_BURST_MESSAGES ? '+' : '-')+ "No burst messages | ";
 
 
-        msg += (this.advancedOptions.CAPABILITIES_NETWORK_ENABLED ? '+' : '-')+ "Network | ";
+        msg += (this.advancedOptions.NETWORK_ENABLED ? '+' : '-')+ "Network | ";
 
-        msg += (this.advancedOptions.CAPABILITIES_SERIAL_NUMBER_ENABLED ? '+' : '-')+ "Serial number | ";
+        msg += (this.advancedOptions.SERIAL_NUMBER_ENABLED ? '+' : '-')+ "Serial number | ";
 
-        msg += (this.advancedOptions.CAPABILITIES_PER_CHANNEL_TX_POWER_ENABLED ? '+' : '-')+ "Per channel Tx Power | ";
+        msg += (this.advancedOptions.PER_CHANNEL_TX_POWER_ENABLED ? '+' : '-')+ "Per channel Tx Power | ";
 
-        msg += (this.advancedOptions.CAPABILITIES_LOW_PRIORITY_SEARCH_ENABLED ? '+' : '-')+ "Low priority search | " ;
+        msg += (this.advancedOptions.LOW_PRIORITY_SEARCH_ENABLED ? '+' : '-')+ "Low priority search | " ;
 
-        msg += (this.advancedOptions.CAPABILITIES_SCRIPT_ENABLED ? '+' : '-')+ "Script | ";
+        msg += (this.advancedOptions.SCRIPT_ENABLED ? '+' : '-')+ "Script | ";
 
-        msg += (this.advancedOptions.CAPABILITIES_SEARCH_LIST_ENABLED ? '+' : '-')+ "Search list | ";
+        msg += (this.advancedOptions.SEARCH_LIST_ENABLED ? '+' : '-')+ "Search list | ";
 
 
         if (this.advancedOptions2)        {
-          msg +=(this.advancedOptions2.CAPABILITIES_LED_ENABLED ? '+' : '-')+  "Led | ";
+          msg +=(this.advancedOptions2.LED_ENABLED ? '+' : '-')+  "Led | ";
 
-          msg += (this.advancedOptions2.CAPABILITIES_EXT_MESSAGE_ENABLED ? '+' : '-')+ "Extended messages | ";
+          msg += (this.advancedOptions2.EXT_MESSAGE_ENABLED ? '+' : '-')+ "Extended messages | ";
 
-          msg += (this.advancedOptions2.CAPABILITIES_SCAN_MODE_ENABLED ? '+' : '-')+ "Scan mode | ";
+          msg += (this.advancedOptions2.SCAN_MODE_ENABLED ? '+' : '-')+ "Scan mode | ";
 
-          msg += (this.advancedOptions2.CAPABILITIES_PROXY_SEARCH_ENABLED ? '+' : '-')+ "Proximity search | ";
+          msg += (this.advancedOptions2.PROXY_SEARCH_ENABLED ? '+' : '-')+ "Proximity search | ";
 
-          msg += (this.advancedOptions2.CAPABILITIES_EXT_ASSIGN_ENABLED ? '+' : '-')+"Extended assign | ";
+          msg += (this.advancedOptions2.EXT_ASSIGN_ENABLED ? '+' : '-')+"Extended assign | ";
 
-          msg += (this.advancedOptions2.CAPABILITIES__FS_ANTFS_ENABLED ? '+' : '-')+ "ANT-FS | ";
+          msg += (this.advancedOptions2._FS_ANTFS_ENABLED ? '+' : '-')+ "ANT-FS | ";
 
         }
 
         if (this.advancedOptions3)        {
-            msg +=  (this.advancedOptions3.CAPABILITIES_ADVANCED_BURST_ENABLED ? '+' : '-')+ 'Advanced burst |';
+            msg +=  (this.advancedOptions3.ADVANCED_BURST_ENABLED ? '+' : '-')+ 'Advanced burst |';
 
-            msg += (this.advancedOptions3.CAPABILITIES_EVENT_BUFFERING_ENABLED ? '+' : '-')+"Event buffering | ";
+            msg += (this.advancedOptions3.EVENT_BUFFERING_ENABLED ? '+' : '-')+"Event buffering | ";
 
-            msg += (this.advancedOptions3.CAPABILITIES_EVENT_FILTERING_ENABLED ? '+' : '-')+"Event filtering | ";
+            msg += (this.advancedOptions3.EVENT_FILTERING_ENABLED ? '+' : '-')+"Event filtering | ";
 
-            msg += (this.advancedOptions3.CAPABILITIES_HIGH_DUTY_SEARCH_ENABLED ? '+' : '-')+"High duty search | ";
+            msg += (this.advancedOptions3.HIGH_DUTY_SEARCH_ENABLED ? '+' : '-')+"High duty search | ";
 
-            msg +=  (this.advancedOptions3.CAPABILITIES_SEARCH_SHARING_ENABLED ? '+' : '-')+"Search sharing | ";
+            msg +=  (this.advancedOptions3.SEARCH_SHARING_ENABLED ? '+' : '-')+"Search sharing | ";
 
-            msg += (this.advancedOptions3.CAPABILITIES_ENCRYPTED_CHANNEL_ENABLED ? '+' : '-')+"Encrypted channel | ";
+            msg += (this.advancedOptions3.ENCRYPTED_CHANNEL_ENABLED ? '+' : '-')+"Encrypted channel | ";
 
-            msg += (this.advancedOptions3.CAPABILITIES_SELECTIVE_DATA_ENABLED  ? '+' : '-')+"Selective data update | ";
+            msg += (this.advancedOptions3.SELECTIVE_DATA_ENABLED  ? '+' : '-')+"Selective data update | ";
 
         }
 
         if (this.advancedOptions4)        {
-          msg += (this.advancedOptions4.CAPABILITIES_RFACTIVE_NOTIFICATION_ENABLED ? '+' : '-')+"RF Active notification | ";
+          msg += (this.advancedOptions4.RFACTIVE_NOTIFICATION_ENABLED ? '+' : '-')+"RF Active notification | ";
 
         }
 
-        if (this.standardOptions !== undefined) msg += 'S.O '+this.standardOptions.value+ ' |';
-        if (this.advancedOptions !== undefined) msg += ' A.O '+this.advancedOptions.value+'|';
-        if (this.advancedOptions2 !== undefined) msg += ' A.O2 '+this.advancedOptions2.value+'|';
-        if (this.advancedOptions3 !== undefined) msg += ' A.O3 '+this.advancedOptions3.value+'|';
-        if (this.advancedOptions4 !== undefined) msg += ' A.O4 '+this.advancedOptions4.value;
+        if (this.standardOptions !== undefined) msg += 'S.O '+this.standardOptions.toString(2) + "b = " + this.standardOptions+ '|';
+        if (this.advancedOptions !== undefined) msg += ' A.O '+this.advancedOptions.toString(2) + "b = " + this.advancedOptions+'|';
+        if (this.advancedOptions2 !== undefined) msg += ' A.O2 '+this.advancedOptions2.toString(2) + "b = " + this.advancedOptions2+'|';
+        if (this.advancedOptions3 !== undefined) msg += ' A.O3 '+this.advancedOptions3.toString(2) + "b = " + this.advancedOptions3+'|';
+        if (this.advancedOptions4 !== undefined) msg += ' A.O4 '+this.advancedOptions4.toString(2) + "b " + this.advancedOptions4;
 
         return msg;
     };
--- messages/requested/ChannelIdMessage.js
+++ messages/requested/ChannelIdMessage.js
@@ -1,16 +1,14 @@
 /* global define: true, DataView: true */
 
-if (typeof define !== 'function'){ var define = require('amdefine')(module); }
-
+if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function (require, exports, module){
 
       'use strict';
 
   var Message = require('../Message'),
-      //Channel = require('../../channel.js');
       ChannelId = require('./channelId');
 
-  function ChannelIdMessage(){
+  function ChannelIdMessage()  {
 
       Message.call(this,undefined,Message.prototype.CHANNEL_ID);
 
@@ -20,52 +18,18 @@ define(function (require, exports, module){
 
   ChannelIdMessage.prototype.constructor = ChannelIdMessage;
 
-  //ChannelIdMessage.prototype.STATE = {
-  //    UN_ASSIGNED: 0x00,
-  //    ASSIGNED: 0x01,
-  //    SEARCHING: 0x02,
-  //    TRACKING: 0x03
-  //}
 
-  ChannelIdMessage.prototype.decode = function (data){
+  ChannelIdMessage.prototype.decode = function (data)  {
 
       this.channelNumber = this.content[0];
       this.channelId = new ChannelId(new DataView(this.content).getUint16(1,true), this.content[3], this.content[4]);
 
-      //var status;
-
-      //this.channelNumber = this.content[0];
-
-      //status = this.content[1];
-      //this.channelStatus = {
-      //    value: status,
-      //    state: status & parseInt("00000011", 2), // Lower 2 bits
-      //    networkNumber: (status & parseInt("00001100", 2)) >> 2,
-      //    channelType: (status & parseInt("11110000", 2)) >> 4
-      //};
-
-      //// Tip from http://www.i-programmer.info/programming/javascript/2550-javascript-bit-manipulation.html
-
-      //switch (this.channelStatus.state)//{
-      //    case ChannelIdMessage.prototype.STATE.UN_ASSIGNED: this.channelStatus.stateMessage = "UN-ASSIGNED"; break;
-      //    case ChannelIdMessage.prototype.STATE.ASSIGNED: this.channelStatus.stateMessage = "ASSIGNED"; break;
-      //    case ChannelIdMessage.prototype.STATE.SEARCHING: this.channelStatus.stateMessage = "SEARCHING"; break;
-      //    case ChannelIdMessage.prototype.STATE.TRACKING: this.channelStatus.stateMessage = "TRACKING"; break;
-      //    default: throw new Error('Unknown state for channel ' + this.channelStatus.state); break;
-      //}
-
-      ////console.log("Channel status", this.channelNumber, this.channelStatus);
-
-      ////this.message = { 'text': this.channelStatus.stateMessage };
-
-      ////return this.message;
-
   };
 
-  ChannelIdMessage.prototype.toString = function (){
+  ChannelIdMessage.prototype.toString = function ()  {
       return Message.prototype.toString.call(this)+ " C# " + this.channelNumber + " " + this.channelId.toString();
   };
 
-  module.exports = ChannelIdMessage;
-      return module.exports;
+    module.exports = ChannelIdMessage;
+    return module.exports;
 });
